d32ac54d309e8bbe2c65f7cbb3fc9bad22fd08b3
imingyu <mingyuhisoft@163.com>
2024-02-25 14:26:10 +0800

    feat: boundingClientRect添加非空判断并添加retryCount参数

--- src/xml.ts
+++ src/xml.ts
@@ -1,7 +1,15 @@
 import type { CrossMpClientRect } from './types';
 import { getApiVar } from './var';
 
-const boundingClientRect = (mode: 'select' | 'selectAll', selector: string, ctx?: any, timeout?: number) => {
+const boundingClientRect = (
+    mode: 'select' | 'selectAll',
+    selector: string,
+    ctx?: any,
+    timeout?: number,
+    retryCount?: number,
+    retryDelay?: number
+    // eslint-disable-next-line max-params
+) => {
     let apiVar = getApiVar();
     if (typeof ctx === 'object' && 'createSelectorQuery' in ctx) {
         apiVar = ctx;
@@ -13,39 +21,61 @@ const boundingClientRect = (mode: 'select' | 'selectAll', selector: string, ctx?
                 reject(new Error('timeout'));
             }, timeout);
         }
-        try {
-            apiVar
-                .createSelectorQuery()
-                // eslint-disable-next-line no-unexpected-multiline
-                [mode](selector)
-                .boundingClientRect((rect) => {
-                    clearTimeout(timer);
-                    if (mode === 'selectAll' && !rect) {
-                        resolve([]);
-                    } else {
-                        resolve(rect);
-                    }
-                })
-                .exec();
-        } catch (error) {
-            clearTimeout(timer);
-            reject(error);
-        }
+        const fire = () => {
+            try {
+                apiVar
+                    .createSelectorQuery()
+                    // eslint-disable-next-line no-unexpected-multiline
+                    [mode](selector)
+                    .boundingClientRect((rect) => {
+                        if (rect) {
+                            clearTimeout(timer);
+                            resolve(rect);
+                            return;
+                        }
+                        if (!retryCount) {
+                            clearTimeout(timer);
+                            reject(new Error('not result'));
+                            return;
+                        }
+                        retryCount--;
+                        if (retryCount < 0) {
+                            clearTimeout(timer);
+                            reject(new Error('not result'));
+                            return;
+                        }
+                        setTimeout(fire, retryDelay || 0);
+                    })
+                    .exec();
+            } catch (error) {
+                clearTimeout(timer);
+                reject(error);
+            }
+        };
+        fire();
     });
 };
 
 export const selectBoundingClientRect = <TDataSet>(
     selector: string,
     ctx?: any,
-    timeout?: number
+    timeout?: number,
+    retryCount = 0,
+    retryDelay = 100
 ): Promise<CrossMpClientRect<TDataSet>> => {
-    return boundingClientRect('select', selector, ctx, timeout) as Promise<CrossMpClientRect<TDataSet>>;
+    return boundingClientRect('select', selector, ctx, timeout, retryCount, retryDelay) as Promise<
+        CrossMpClientRect<TDataSet>
+    >;
 };
 
 export const selectAllBoundingClientRect = (
     selector: string,
     ctx?: any,
-    timeout?: number
+    timeout?: number,
+    retryCount = 0,
+    retryDelay = 100
 ): Promise<CrossMpClientRect[]> => {
-    return boundingClientRect('selectAll', selector, ctx, timeout) as Promise<CrossMpClientRect[]>;
+    return boundingClientRect('selectAll', selector, ctx, timeout, retryCount, retryDelay) as Promise<
+        CrossMpClientRect[]
+    >;
 };
